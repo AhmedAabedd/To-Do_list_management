71c381d00456462b4008b96d38dda3537a4f971b
Ahmed Abed <ahmed.labedd@gmail.com>
2025-06-17 19:33:56 +0100

    Initial commit of ToDo module

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'To-Do List',
+    'version' : '1.0',
+    'summary': 'Todo list management',
+    'sequence': 10,
+    'description': '"To-Do list management software"',
+    'category': 'Productivity',
+    'website': 'https://www.proosoftcloud.com/',
+    'depends' : [],
+    'data': ['security/ir.model.access.csv',
+             'views/menu.xml',
+             'views/task_view.xml',
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+}
---
+++ models/task.py
@@ -0,0 +1,73 @@
+from odoo import models, fields, api, _
+from datetime import date
+
+
+
+class ToDoTask(models.Model):
+    _name = 'todo.task'
+
+
+    name = fields.Char(string="Task Name", required=1)
+    assign_to = fields.Many2one('res.partner', string="Assign To", required=True)
+    description = fields.Text(string="Description")
+    due_date = fields.Date(string="Due Date")
+    state = fields.Selection([
+        ('new', 'New'),
+        ('inprogress', 'In Progress'),
+        ('paused', 'Paused'),
+        ('completed', 'Completed'),
+        ('cancelled', 'cancelled')
+    ], default="new")
+
+    remaining_state = fields.Selection([
+        ('normal', 'Normal'),
+        ('warning', 'Warning'),
+        ('danger', 'Danger')
+    ], default="normal", compute="_compute_remaining_state", store=1)
+
+    deadline_date = fields.Char(string="Deadline", compute="_compute_deadline_date", store=1)
+
+
+    def action_in_progress(self):
+        for rec in self:
+            rec.state = 'inprogress'
+    
+    def action_paused(self):
+        for rec in self:
+            rec.state = 'paused'
+    
+    def action_completed(self):
+        for rec in self:
+            rec.state = 'completed'
+
+    def action_cancelled(self):
+        for rec in self:
+            rec.state = 'cancelled'
+    
+    @api.depends('due_date')
+    def _compute_remaining_state(self):
+        today = date.today()
+        for rec in self:
+            if rec.due_date:
+                delta = (rec.due_date - today).days
+                if delta == 0 or delta == 1 :
+                    rec.remaining_state = 'warning'
+                elif delta < 0 :
+                    rec.remaining_state = 'danger'
+
+    @api.depends('due_date')
+    def _compute_deadline_date(self):
+        today = date.today()
+        for rec in self:
+            if rec.due_date:
+                delta = (rec.due_date - today).days
+                if delta == 0:
+                    rec.deadline_date = "Today"
+                elif delta == 1:
+                    rec.deadline_date = "Tomorrow"
+                elif delta > 1:
+                    rec.deadline_date = f"In {delta} days"
+                elif delta == -1:
+                    rec.deadline_date = "Yesterday"
+                else:
+                    rec.deadline_date = f"{abs(delta)} days ago"
